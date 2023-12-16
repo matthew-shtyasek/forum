d0c0daffe63142fabcc86bc735d56cbf9e5a71ff
abcdefg <43@mail.ru>
2023-12-16 12:07:25 +0500

    Add app forum_profile
    Add model Profile

--- forum_profile/admin.py
+++ forum_profile/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from forum_profile.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    exclude = ['id']
+    search_fields = ['username',
+                     'description',
+                     'signature',
+                     'profile_color']
+    list_display = ['username',
+                    'photo',
+                    'profile_color',
+                    'user']
+
+    list_editable = ['photo',
+                     'profile_color']
--- forum_profile/models.py
+++ forum_profile/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='profile/photo/%Y/%m/%d')
+    username = models.CharField(max_length=16)
+    description = models.TextField()
+    signature = models.TextField()
+    profile_color = models.CharField(max_length=8, validators=[MinLengthValidator(8)])
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('username',)
